c641c18d83c01b483dfda7810864a3e8ffaa0ec6
wojteksyk <144209174+wojteksyk@users.noreply.github.com>
2024-09-23 14:01:38 +0200

    23.09

--- untitled/server.js
+++ untitled/server.js
@@ -1,19 +1,17 @@
 const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
-var url = require("url");
+const url = require("url");
 const port = 3000;
 const fs = require("fs");
 const adr = "http://127.0.0.1:3000/get_params"
 const q = url.parse(adr,true);
 const server = createServer((req, res) => {
     const url = req.url;
-    if(url==="/"){
+    if (url === "/home") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('strona glowna');
-    }
-
-    else  if(url==="/json"){
+    } else if (url === "/json") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         console.log('plik .json');
@@ -26,28 +24,55 @@ const server = createServer((req, res) => {
         });
 
 
-
-    }
-    else  if(url==="/html-json"){
+    } else if (url === "/html-json") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('dokument HTML generowany wewnątrz kodu Node.js');
-    }
-    else  if(url==="/html"){
+    } else if (url === "/html") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('dokument HTML pobrany z pliku');
     }
-    else  if(url==="/get_params"){
+    /*else  if(url==="/get_params"){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
        res.write(q.host+"    ");
         res.write(q.pathname+"       ");
         res.end(q.method);
+    }*/
+
+    else if (req.method === 'GET' && url.startsWith('/get_params')){
+        const url = require('url');
+
+    const parsedUrl = url.parse(req.url, true);
+
+    console.log(parsedUrl.query);
+
+    fs.writeFile("./parametry.json", JSON.stringify(parsedUrl.query, null, 4), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            console.log("Zapisano do pliku");
+        }
+    });
+
+    res.end('Parametry wczytane');
+
+}
 
 
 
-
+    else{
+    const url = require("url");
+        const file = "dupa.html";
+        const parsedUrl = url.parse(req.url, true);
+        if(fs.existsSync("assets/dupa.html")){
+console.log("file found");
+        }
+        else{
+            console.log("file not found");
+        }
 
     }
 
